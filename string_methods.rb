dd4c2d5fdbf7a1a810c5991309c9f5de0962958c
Manoharkshetty <manoharkshetty@gmail.com>
2014-06-19 16:07:58 +0530

    worked on string methods

---
+++ string_methods.rb
@@ -0,0 +1,27 @@
+str="hello world"
+
+p str *3
+
+
+p "==="*10
+p "hello" + " " + "world!"
+p "hello" << " world!"
+p "hello ".concat "world!"
+
+
+p "==="*10
+p str.count "lo"
+p str.length
+p str.size
+p str.count "l"
+
+
+p "==="*10
+str2= "Hello Ruby!"
+p str2.upcase
+p str2
+p str.upcase!
+p str
+
+str3="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam perferendis optio alias qui enim incidunt neque quis impedit, eaque, porro architecto dolor vel laboriosam velit repellendus dolorum explicabo modi error."
+p str3.split " "
