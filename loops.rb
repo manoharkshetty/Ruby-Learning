c4fbba160ff65863da87417647c5e04ad087b1fb
Manoharkshetty <manoharkshetty@gmail.com>
2014-06-19 09:03:14 +0530

    while loop

--- loops.rb
+++ loops.rb
@@ -1,6 +1,11 @@
 number=1;
-loop do 
-	break if number>15
+# loop do 
+# 	break if number>15
+# 	puts "the number is #{number}"
+# 	number=number+1
+# end
+
+while number<16 do 
 	puts "the number is #{number}"
 	number=number+1
 end
