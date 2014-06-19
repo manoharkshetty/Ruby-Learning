e78b5a1401c93e8ec026d532e81a3821f9532d3b
Manoharkshetty <manoharkshetty@gmail.com>
2014-06-19 10:56:21 +0530

    work on proc

---
+++ proc_lambda.rb
@@ -0,0 +1,24 @@
+def form_proc p
+	puts "<form>"
+	p.call true
+	puts "</form>"
+end
+
+
+
+def paragraph para
+	puts "<p>" + para +"</p>"
+end
+
+def quotes str
+	puts "<blockquote>" + str + "</blockquote>"
+end
+
+myproc= proc do |only_quotes|
+	paragraph "i am th paragraph" unless only_quotes
+		
+	
+	quotes "this is in quotes"
+	end
+
+form_proc myproc
