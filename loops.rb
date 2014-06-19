bdab5c1f06ebe992036ab1cf6490326d7010cacf
Manoharkshetty <manoharkshetty@gmail.com>
2014-06-19 09:11:59 +0530

    fixnum.times,range.each

--- loops.rb
+++ loops.rb
@@ -1,11 +1,24 @@
-number=1;
+# number=1;
 # loop do 
 # 	break if number>15
 # 	puts "the number is #{number}"
 # 	number=number+1
 # end
 
-while number<16 do 
-	puts "the number is #{number}"
-	number=number+1
-end
+# while number<16 do 
+# 	puts "the number is #{number}"
+# 	number=number+1
+# end
+
+# 15.times do |number|
+# 	puts "the number is #{number}"
+# end
+
+# (1..15).each do |number|
+#  	puts "the number is #{number}"
+#  end
+list=[1,2,3,4,5,6,7,8,9,10]
+list.each do |number|
+ 	puts "the number is #{number}"
+ end
+	
